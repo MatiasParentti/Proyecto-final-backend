58974c21a5a8959e6b3c33d6b94a77cb44ac8024
MatiasParentti <matiparentti@gmail.com>
2025-07-13 15:21:02 -0300

    hash contraseña

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,10 +1,13 @@
 const db = require("../config/db");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
-const SECRET_KEY = process.env.SECRET_KEY;
 
-const registerUser = (email, passwordHash, rol = "usuario") => {
-  if (!email || !passwordHash)
+const SECRET_KEY = process.env.SECRET_KEY;
+const SALT_ROUNDS = 10;
+
+const registerUser = (email, password, rol = "usuario") => {
+  if (!email || !password)
     throw new Error("Email y contraseña son obligatorios");
   if (rol !== "admin" && rol !== "usuario") throw new Error("Rol inválido");
 
@@ -12,6 +15,9 @@ const registerUser = (email, passwordHash, rol = "usuario") => {
     .prepare("SELECT COUNT(*) AS total FROM usuarios WHERE email = ?")
     .get(email);
   if (exists.total > 0) throw new Error("El email ya está registrado");
+
+  // 🔒
+  const passwordHash = bcrypt.hashSync(password, SALT_ROUNDS);
 
   db.prepare(
     "INSERT INTO usuarios (email, password_hash, rol) VALUES (?, ?, ?)"
@@ -21,11 +27,14 @@ const registerUser = (email, passwordHash, rol = "usuario") => {
 const loginUser = (email, password) => {
   const user = db
     .prepare(
-      "SELECT id, email, rol FROM usuarios WHERE email = ? AND password_hash = ?"
+      "SELECT id, email, password_hash, rol FROM usuarios WHERE email = ?"
     )
-    .get(email, password);
+    .get(email);
 
   if (!user) throw new Error("Credenciales inválidas");
+
+  const validPassword = bcrypt.compareSync(password, user.password_hash);
+  if (!validPassword) throw new Error("Credenciales inválidas");
 
   const token = jwt.sign(
     { id: user.id, email: user.email, rol: user.rol },
